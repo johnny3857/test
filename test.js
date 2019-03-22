eb2a7386405e0e6b286f100bff4fc139cadb886b
Johnny <johnny@home.com>
2019-03-23 00:26:20 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,21 @@
+document.getElementById('btn-word').addEventListener('click', createWord);
+
+console.log("Hello world!");
+const mgh = ['a', 'e', 'i', 'o', 'u'];
+const msh = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's', 't', 'v', 'x', 'z'];
+
+let w = '';
+let c;
+
+
+function createWord() {
+  w = '';
+  for (i = 0; i < 4; i++) {
+    c = Math.floor(Math.random() * msh.length);
+    w += msh[c];
+    c = Math.floor(Math.random() * mgh.length);
+    w += mgh[c];
+  }
+  console.log(w);
+  document.querySelector('h3').innerText = w;
+}
