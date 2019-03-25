508500200f6b4484fc512dd4ac5089c4cd773b84
Johnny <johnny@home.net>
2019-03-25 13:57:36 +0100

    update01

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 document.getElementById('btn-word').addEventListener('click', createWord);
 
 console.log("Hello world!");
-const mgh = ['a', 'e', 'i', 'o', 'u'];
-const msh = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's', 't', 'v', 'x', 'z'];
+const mgh = ['a', 'e', 'i', 'o', 'u', 'á', 'é', 'í', 'ó', 'ö', 'ü'];
+const msh = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's', 't', 'v', 'z', 'cs', 'gy', 'ly', 'ny', 'sz', 'ty', 'zs', 'dzs'];
 
 let w = '';
 let c;
